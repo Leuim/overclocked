bf181ab04e2f519f976c7b24faf8d475f96724bc
leuim <jassim.mohammed48@gmail.com>
2025-09-10 04:45:47 +0300

    added the initial models and registered them in the admin dashboard

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Cart, Cartitem, Product, Order, Category
 # Register your models here.
+admin.site.register(Cart)
+admin.site.register(Cartitem)
+admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(Category)
